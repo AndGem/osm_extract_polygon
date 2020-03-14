efe6f4fc5b4d43c3010b7fea5bd8e29ec9a77815
Andreas <andreas.gemsa@googlemail.com>
2020-03-14 21:24:33 +0100

    minor improvements

--- src/converter.rs
+++ src/converter.rs
@@ -34,7 +34,6 @@ pub fn convert(relations: Vec<RelationNodes>) -> Vec<Polygon> {
 }
 
 fn merge_nodes(rn: RelationNodes) -> RelationNodes {
-    //  -> RelationNodes
     /*
         merging of nodes is necessary because ways are split into multiple groups
         assumption:
@@ -44,7 +43,7 @@ fn merge_nodes(rn: RelationNodes) -> RelationNodes {
          1. start with first way and iterate over the rest of nodes and try to find a match
            - if yes -> merge
            - if no -> go to next
-         2. repeat process until
+         2. repeat process until nothing to merge
     */
 
     let mut nodes = rn.nodes;
@@ -54,28 +53,24 @@ fn merge_nodes(rn: RelationNodes) -> RelationNodes {
         let mut path: Vec<Node> = nodes.swap_remove(0);
 
         loop {
-            // let mut found_something = false;
-            let (matching_first, x) = find_match(path.first().unwrap().id, nodes);
-            nodes = x; //TODO: there must be a nicer way to do this!
+            let matching_first = find_match(path.first().unwrap().id, &mut nodes);
 
             if let Some(mut matching_nodes) = matching_first {
-                // found_something = true;
                 matching_nodes.reverse();
-                path = [&matching_nodes[..], &path[..]].concat();
+                matching_nodes.append(&mut path);
+                path = matching_nodes;
                 continue;
             }
 
-            let (matching_last, x) = find_match(path.last().unwrap().id, nodes);
-            nodes = x; //TODO: there must be a nicer way to do this!
+            let matching_last = find_match(path.last().unwrap().id, &mut nodes);
 
-            if let Some(matching_nodes) = matching_last {
-                path = [&path[..], &matching_nodes[..]].concat();
+            if let Some(mut matching_nodes) = matching_last {
+                path.append(&mut matching_nodes);
                 continue;
             }
 
             break;
         }
-        //
 
         result_nodes.push(path);
     }
@@ -86,7 +81,7 @@ fn merge_nodes(rn: RelationNodes) -> RelationNodes {
     }
 }
 
-fn find_match(node_id: NodeId, mut nodes: Vec<Vec<Node>>) -> (Option<Vec<Node>>, Vec<Vec<Node>>) {
+fn find_match(node_id: NodeId, nodes: &mut Vec<Vec<Node>>) -> Option<Vec<Node>> {
     /*
         n_id, [------, n_id-----, -----]
         => Some(n_id-----), [------, -----]
@@ -100,14 +95,14 @@ fn find_match(node_id: NodeId, mut nodes: Vec<Vec<Node>>) -> (Option<Vec<Node>>,
         }
         if node.first().unwrap().id == node_id {
             let result = nodes.swap_remove(i);
-            return (Some(result), nodes);
+            return Some(result);
         } else if node.last().unwrap().id == node_id {
             let mut result = nodes.swap_remove(i);
             result.reverse();
-            return (Some(result), nodes);
+            return Some(result);
         }
     }
-    (None, nodes)
+    None
 }
 
 fn convert_to_poly(rn: RelationNodes) -> Polygon {
