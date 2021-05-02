88f86c5c277f3fd3fbcb1994076c9ae93ce506c2
Andreas <andreas.gemsa@googlemail.com>
2021-05-02 14:38:44 +0200

    minor cleanup

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,8 +1,8 @@
-pub mod file_writer_geojson;
-pub mod file_writer_poly;
 pub mod output_handler;
 
 mod file_creator;
+mod file_writer_geojson;
+mod file_writer_poly;
 
 pub enum OverwriteConfiguration {
     Ask,
