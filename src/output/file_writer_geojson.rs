f338732a35006e2cde47b22609008f2868741790
Andreas <andreas.gemsa@googlemail.com>
2021-05-08 15:23:37 +0200

    Geojson support add output (#20)
    * added geojson output support

--- src/output/file_writer_geojson.rs
+++ src/output/file_writer_geojson.rs
@@ -1,13 +1,50 @@
-use crate::converter::Polygon;
+use crate::converter::{Point, Polygon};
 use crate::output::output_handler::FileWriter;
 
+use geo_types::Polygon as GeoPolygon;
+use geo_types::{Coordinate, LineString, MultiPolygon};
+use geojson::Geometry;
+
 use std::fs::File;
+use std::io::prelude::*;
+
+use serde_json::json;
+use serde_json::map::Map;
 
 pub struct GeoJsonWriter {}
 
 impl FileWriter for GeoJsonWriter {
     fn write_to_file(&self, file: &mut File, polygon: &Polygon) -> std::io::Result<()> {
+        let vec_polygons = convert_polygon_to_geo_polygons(polygon);
+        let multipolygon = MultiPolygon(vec_polygons);
+
+        //TODO: add admin level for boundaries
+        let mut foreign_members = Map::new();
+        foreign_members.insert("name".to_string(), json!(polygon.name));
+
+        let geometry = Geometry {
+            bbox: None,
+            value: geojson::Value::from(&multipolygon),
+            foreign_members: Some(foreign_members),
+        };
+
+        file.write_all(geometry.to_string().as_bytes())?;
+
         Ok(())
-        //TODO: fill in code to write geojson
     }
 }
+
+fn convert_polygon_to_geo_polygons(polygon: &Polygon) -> Vec<GeoPolygon<f32>> {
+    polygon
+        .points
+        .iter()
+        .map(|points| convert_to_linestring(points))
+        .map(|linestring| GeoPolygon::new(linestring, vec![]))
+        .collect()
+}
+
+fn convert_to_linestring(points: &[Point]) -> LineString<f32> {
+    LineString(points.iter().map(|p| Coordinate { x: p.lon, y: p.lat }).collect())
+}
+
+//TODO: write unit tests
