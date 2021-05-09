3488fbcf28b3dd9bc1ab53e01f1762883e4c7908
Andreas <andreas.gemsa@googlemail.com>
2021-05-09 19:12:32 +0200

    performance improvement for writing poly files (#21)

--- src/output/file_writer_poly.rs
+++ src/output/file_writer_poly.rs
@@ -1,6 +1,7 @@
 use crate::converter::Polygon;
 use crate::output::output_handler::FileWriter;
 
+use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -8,20 +9,25 @@ pub struct PolyWriter {}
 
 impl FileWriter for PolyWriter {
     fn write_to_file(&self, file: &mut File, polygon: &Polygon) -> std::io::Result<()> {
-        file.write_all(&polygon.name.as_bytes())?;
-        file.write_all(b"\n")?;
+        let mut output: String = String::new();
+        output.push_str(&polygon.name);
+        output.push('\n');
+
         let mut index: i32 = 1;
 
-        //TODO: improve performance by preparing output
         for points in &polygon.points {
-            file.write_fmt(format_args!("area_{}\n", index))?;
+            let area_id_str = fmt::format(format_args!("area_{}\n", index));
+            output.push_str(&area_id_str);
             for point in points {
-                file.write_fmt(format_args!("\t{} \t{}\n", point.lon, point.lat))?;
+                let point_str = fmt::format(format_args!("\t{} \t{}\n", point.lon, point.lat));
+                output.push_str(&point_str);
             }
-            file.write_all(b"END\n")?;
+            output.push_str("END\n");
             index += 1;
         }
-        file.write_all(b"END\n")?;
+        output.push_str("END\n");
+        file.write_all(output.as_bytes())?;
+
         Ok(())
     }
 }
