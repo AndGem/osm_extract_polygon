5749c3cd1363ca10f944de7f3e76773924c05336
Andreas <andreas.gemsa@googlemail.com>
2021-05-13 10:59:24 +0200

    refactored and added test

--- src/output/file_writer_geojson.rs
+++ src/output/file_writer_geojson.rs
@@ -15,35 +15,47 @@ pub struct GeoJsonWriter {}
 
 impl FileWriter for GeoJsonWriter {
     fn write_to_file(&self, file: &mut File, polygon: &Polygon) -> std::io::Result<()> {
-        let vec_polygons = convert_polygon_to_geo_polygons(polygon);
+        let feature = convert_polygon_to_geojson_feature(polygon);
+        if feature.is_ok() {
+            Ok(file.write_all(feature.unwrap().to_string().as_bytes())?)
+        } else {
+            Err(Error::new(ErrorKind::Other, "Error in converting Polygon to GeoJSON."))
+        }
+    }
+}
 
-        let geometry = match vec_polygons.len() {
-            0 => {
-                println!(
-                    "Error in converting Polygon to GeoJSON: {}. Doesn't contain points",
-                    polygon.name
-                );
-                return Err(Error::new(ErrorKind::Other, "Error in converting Polygon to GeoJSON."));
-            }
-            1 => Geometry::new(geojson::Value::from(vec_polygons.get(0).unwrap())),
-            _ => Geometry::new(geojson::Value::from(&MultiPolygon(vec_polygons))),
-        };
+fn convert_polygon_to_geojson_feature(polygon: &Polygon) -> Result<Feature, ()> {
+    let properties = create_properties(polygon);
+    let polygons = convert_polygon_to_geo_polygons(polygon);
 
-        //TODO: add admin level for boundaries
-        let mut properties = Map::new();
-        properties.insert(String::from("name"), to_value(&polygon.name).unwrap());
+    convert_to_geometry(polygons).and_then(|p| convert_to_feature(p, properties))
+}
 
-        let geojson = Feature {
-            bbox: None,
-            geometry: Some(geometry),
-            id: None,
-            properties: Some(properties),
-            foreign_members: None,
-        };
+fn convert_to_feature(geometry: Geometry, properties: Map<String, serde_json::Value>) -> Result<Feature, ()> {
+    let feature = Feature {
+        bbox: None,
+        geometry: Some(geometry),
+        id: None,
+        properties: Some(properties),
+        foreign_members: None,
+    };
 
-        file.write_all(geojson.to_string().as_bytes())?;
+    Ok(feature)
+}
 
-        Ok(())
+fn create_properties(polygon: &Polygon) -> Map<String, serde_json::Value> {
+    //TODO: add admin level for boundaries
+    let mut properties = Map::new();
+    properties.insert(String::from("name"), to_value(&polygon.name).unwrap());
+
+    properties
+}
+
+fn convert_to_geometry(polygons: Vec<GeoPolygon<f32>>) -> Result<Geometry, ()> {
+    match polygons.len() {
+        0 => Err(()),
+        1 => Ok(Geometry::new(geojson::Value::from(polygons.get(0).unwrap()))),
+        _ => Ok(Geometry::new(geojson::Value::from(&MultiPolygon(polygons)))),
     }
 }
 
@@ -60,4 +72,35 @@ fn convert_to_linestring(points: &[Point]) -> LineString<f32> {
     LineString(points.iter().map(|p| Coordinate { x: p.lon, y: p.lat }).collect())
 }
 
-//TODO: write unit tests
+// ////////////////////////////////////
+// ////////////////////////////////////
+// UNIT TESTS
+// ////////////////////////////////////
+// ////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_convert_single_polygon_to_geo_polygons() {
+        let p1 = Point { lat: 1.0, lon: 1.0 };
+        let p2 = Point { lat: 2.0, lon: 10.0 };
+        let p3 = Point { lat: 3.0, lon: 100.0 };
+
+        let single_polygon = Polygon {
+            name: "barfoo".to_string(),
+            points: vec![vec![p1.clone(), p2.clone(), p3.clone()]],
+        };
+
+        let result = convert_polygon_to_geo_polygons(&single_polygon);
+        let expected_line_str = LineString(vec![
+            Coordinate { x: p1.lon, y: p1.lat },
+            Coordinate { x: p2.lon, y: p2.lat },
+            Coordinate { x: p3.lon, y: p3.lat },
+        ]);
+        let expected = vec![GeoPolygon::new(expected_line_str, vec![])];
+
+        assert_eq!(result, expected);
+    }
+}
