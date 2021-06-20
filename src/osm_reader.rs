a889a004d8dfed1ab7d0a41726c02d580952d4f8
Andreas <andreas.gemsa@googlemail.com>
2021-06-20 16:25:58 +0200

    first try

--- src/osm_reader.rs
+++ src/osm_reader.rs
@@ -91,9 +91,10 @@ fn find_admin_boundary_relations(
         .par_iter()
         .map(Result::unwrap)
         .filter(|obj| obj.is_relation())
+        // .map(|obj| obj.relation().unwrap().clone())
+        .filter(|obj| obj.relation().unwrap().tags.contains("boundary", "administrative"))
+        .filter(|obj| has_proper_admin_level(obj.relation().unwrap(), min_admin, max_admin))
         .map(|obj| obj.relation().unwrap().clone())
-        .filter(|relation| relation.tags.contains("boundary", "administrative"))
-        .filter(|relation| has_proper_admin_level(relation, min_admin, max_admin))
         .map(|relation| (relation.id, relation))
         .collect();
 
@@ -119,8 +120,9 @@ fn find_nodes_for_way_ids(pbf: &mut OsmPbfReaderFile, way_ids: HashSet<WayId>) -
         .par_iter()
         .map(Result::unwrap)
         .filter(|obj| obj.is_way())
+        // .map(|obj| obj.way().unwrap().clone())
+        .filter(|obj| way_ids.contains(&obj.way().unwrap().id))
         .map(|obj| obj.way().unwrap().clone())
-        .filter(|way| way_ids.contains(&way.id))
         .map(|way| (way.id, way.nodes))
         .collect();
 
@@ -137,8 +139,9 @@ fn find_nodes_for_node_ids(pbf: &mut OsmPbfReaderFile, node_ids: HashSet<NodeId>
         .par_iter()
         .map(Result::unwrap)
         .filter(|obj| obj.is_node())
+        // .map(|obj| obj.node().unwrap().clone())
+        .filter(|obj| node_ids.contains(&obj.node().unwrap().id))
         .map(|obj| obj.node().unwrap().clone())
-        .filter(|node| node_ids.contains(&node.id))
         .map(|node| (node.id, node))
         .collect();
 
