d1f7e833a5180ddc79c4fdd047687077c043e34c
Andreas Gemsa <andreas.gemsa@googlemail.com>
2024-06-03 20:58:57 +0200

    upgraded to clap 4

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate osmpbfreader;
 
 use crate::output::output_handler::OutputHandlerConfiguration;
 use crate::output::OverwriteConfiguration;
-use clap::{crate_authors, crate_version, App, AppSettings, Arg};
+use clap::{crate_authors, crate_version, Arg,command};
 
 mod converter;
 mod osm_reader;
@@ -18,80 +18,81 @@ fn main() {
     const SKIP_ARG: &str = "SKIP";
     const GEOJSON_ARG: &str = "GEOJSON";
 
-    let matches = App::new("OSM Extract Polygon")
+    let matches = command!("OSM Extract Polygon")
         .version(crate_version!())
         .author(crate_authors!())
         .about(
             "Extracts administrative boundaries of OSM pbf files and produces polygon files compatible with Osmosis.",
         )
-        .setting(AppSettings::ArgRequiredElseHelp)
+        // .setting(AppSettings::ArgRequiredElseHelp)
         .arg(
-            Arg::with_name(INPUT_ARG)
+            Arg::new(INPUT_ARG)
                 .short('f')
                 .long("file")
                 .value_name("filename")
                 .help("input file")
                 .required(true)
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name(MIN_ADMIN_LEVEL_ARG)
+            Arg::new(MIN_ADMIN_LEVEL_ARG)
                 .short('m')
                 .long("min")
                 .value_name("min_admin_level")
                 .help("minimum administrative level (can take value from 1-11) [default: 8]")
                 .required(false)
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name(MAX_ADMIN_LEVEL_ARG)
+            Arg::new(MAX_ADMIN_LEVEL_ARG)
                 .short('x')
                 .long("max")
                 .value_name("max_admin_level")
                 .help("max administrative level (can take value from 1-11) [default: 8]")
                 .required(false)
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name(OUTPUT_FOLDER)
+            Arg::new(OUTPUT_FOLDER)
                 .short('p')
                 .long("path")
                 .value_name("path")
                 .help("path to which the output will be saved to [default: '<input_filename>_polygons/']")
                 .required(false)
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name(OVERWRITE_ARG)
+            Arg::new(OVERWRITE_ARG)
             .short('o')
             .long("overwrite")
-            .takes_value(false)
+            .num_args(0)
             .help("set this flag to overwrite files without asking; if neither this nor --skip is set the user is being prompted should a file be overwritten.")
         )
         .arg(
-            Arg::with_name(SKIP_ARG)
+            Arg::new(SKIP_ARG)
             .short('s')
             .long("skip")
-            .takes_value(false)
+            .num_args(0)
             .help("set this flag to skip overwriting files; if neither this nor --overwrite is set the user is being prompted should a file be overwritten.")
         )
         .arg(
-            Arg::with_name(GEOJSON_ARG)
+            Arg::new(GEOJSON_ARG)
             .short('g')
             .long("geojson")
-            .takes_value(false)
+            .num_args(0)
             .help("set this flag to generate geojson output")
         )
         .get_matches();
 
     let min_admin_level = matches
-        .value_of(MIN_ADMIN_LEVEL_ARG)
-        .unwrap_or("8")
+        .get_one::<String>(MIN_ADMIN_LEVEL_ARG)
+        .unwrap_or(&("8".to_string()))
         .parse::<i8>()
         .unwrap();
+
     let max_admin_level = matches
-        .value_of(MAX_ADMIN_LEVEL_ARG)
-        .unwrap_or("8")
+        .get_one::<String>(MAX_ADMIN_LEVEL_ARG)
+        .unwrap_or(&("8".to_string()))
         .parse::<i8>()
         .unwrap();
 
@@ -103,8 +104,8 @@ fn main() {
         std::process::exit(-1);
     }
 
-    let overwrite_all = matches.is_present(OVERWRITE_ARG);
-    let skip_all = matches.is_present(SKIP_ARG);
+    let overwrite_all = matches.contains_id(OVERWRITE_ARG);
+    let skip_all = matches.contains_id(SKIP_ARG);
 
     if overwrite_all && skip_all {
         println!("error: cannot set both -o (--overwrite) and -s (--skip)!");
@@ -119,17 +120,17 @@ fn main() {
         OverwriteConfiguration::Ask
     };
 
-    let geojson_output = matches.is_present(GEOJSON_ARG);
+    let geojson_output = matches.contains_id(GEOJSON_ARG);
 
     let output_handler_config = OutputHandlerConfiguration {
         overwrite_configuration,
         geojson_output,
     };
 
-    let in_filename = matches.value_of(INPUT_ARG).unwrap();
+    let in_filename = matches.get_one::<String>(INPUT_ARG).unwrap();
     println!("Using input file: {}", in_filename);
     let default_path = format!("{}_polygons/", in_filename);
-    let path = matches.value_of(OUTPUT_FOLDER).unwrap_or(&default_path);
+    let path = matches.get_one::<String>(OUTPUT_FOLDER).unwrap_or(&default_path);
     println!("Output path: {}", path);
 
     let relations = osm_reader::read_osm(in_filename, &min_admin_level, &max_admin_level);
